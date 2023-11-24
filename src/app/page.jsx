af7c0116adfbd652f0378e50c773ed29d265dc2f
oRonold <or040918@gmail.com>
2023-11-24 20:38:32 -0300

    Ajuste Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,22 +21,6 @@ export default async function Home() {
           necessidade do paciente a ir se encontrar com o médico pessoalmente 
           correndo o risco da resposta ser simplesmente um “está tudo bem com os seus 
           exames”.</p>
-          <p>Visando simplificar esse processo ou, pelo menos, apresentar uma 
-          alternativa que gere maior comodidade tanto para os pacientes, quanto 
-          vantagens econômicas e melhor aproveitamento dessa estrutura física, a 
-          solução coleta os dados desse exame e, por meio de uma IA é feita uma 
-          análise dos parâmetros contidos no resultado do exame, afim de dar um 
-          parecer mais simplificado sobre o resultado do exame.</p>
-          <p>Uma das partes também interessantes nesse processo, é disponibilizar 
-          uma forma de ser dada uma recomendação médica após o médico ou 
-          laboratório visualizarem esses resultados, diminuindo mais ainda a 
-          necessidade do paciente ter que recorrer a voltar pessoalmente ao hospital só 
-          para uma interpretação dos resultados ou receber um feedback simples do 
-          médico como um “É necessário diminuir o sódio nas suas alimentações e 
-          ingerir mais água”, que seria uma recomendação que poderia otimizar melhor 
-          os recursos do hospital e dos serviços de saúde, se pudessem ser feitos de 
-          maneira online já no ato de disponibilizar o resultado para paciente via 
-          aplicativo ou página web.</p>
         </div>
       </div>
     </main>
